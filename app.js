b9ac425c6e6cf4c83c9937a40b39dceb1f38a23d
Grant Timmerman <granttimmerman@gmail.com>
2013-09-19 10:40:27 -0700

    Adding basic dependencies

---
+++ app.js
@@ -0,0 +1,41 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var consolidate = require('consolidate');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+
+// Handlebars
+app.engine('hbs', consolidate.handlebars);
+app.set('view engine', 'hbs');
+
+// Other
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
