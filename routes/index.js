782ccd70206a14b145e9c1ea40be727d32862205
Grant Timmerman <granttimmerman@gmail.com>
2013-09-19 23:04:25 -0700

    Added more dependencies

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,36 @@
  */
 
 exports.index = function(req, res){
+	var mongoose = require('mongoose');
+	var extend = require('mongoose-schema-extend');
+	var Schema = mongoose.Schema;
+
+	mongoose.connect('mongodb://localhost/test');
+	var db = mongoose.connection;
+
+	db.on('error', console.error.bind(console, 'connection error'));
+	db.once('open', function callback() {
+
+		var PostSchema = new Schema({
+			title: String,
+			thumbnailImageURL: String,
+			fullImageURL: String,
+			description: String,
+			postDate: Date,
+			editDate: Date
+		});
+
+		var NoteSchema = PostSchema.extend({
+			content: String
+		});
+
+		var MediaSchema = PostSchema.extend({
+			link: String
+		});
+
+		var LabSchema = MediaSchema.extend({});
+		var GameSchema = MediaSchema.extend({});
+	});
+
 	res.render('index', { title: 'Express' });
 };
