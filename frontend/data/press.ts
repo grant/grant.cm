c9094c1da36dc98d4b4cc2bea3d94065fa0511f5
Grant Timmerman <granttimmerman@gmail.com>
2021-12-18 17:46:08 -0800

    feat: add press data. fixes #30
    Signed-off-by: Grant Timmerman <granttimmerman@gmail.com>

---
+++ frontend/data/press.ts
@@ -0,0 +1,14 @@
+/**
+ * Metadata about press articles.
+ */
+export type Press = string[];
+
+export const press: Press = [
+  'https://www.stanforddaily.com/2016/02/16/the-will-factor-team-pulse-talks-team-dynamics-and-culture-at-treehacks',
+  'https://www.stanforddaily.com/2016/02/16/a-look-at-the-teams-from-treehacks-2016',
+  'https://www.geekwire.com/2014/inside-dubhacks-uw-hosts-inaugural-collegiate-hackathon',
+  'https://cloud.google.com/blog/products/application-development/introducing-slides-api-codelab',
+  // Slides: t.co/Uy4Ga81GpX
+  'https://www.meetup.com/gdg-silicon-valley/events/244582419/',
+  'https://cloud.google.com/blog/products/application-development/three-new-tools-to-help-improve-your',  
+];
