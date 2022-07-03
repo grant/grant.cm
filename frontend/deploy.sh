6b9db48ffd8c621eebac2e5fa1c9aa6d45aeeabd
Grant Timmerman <granttimmerman@gmail.com>
2022-07-02 19:03:18 -0700

    ci: do not deploy when builds fail locally
    Signed-off-by: Grant Timmerman <granttimmerman@gmail.com>

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -15,9 +15,17 @@ echo "Deploying to project: ${SERVICE}"
 # Build the app
 npm run build;
 
-# Deploys the web app to Cloud Run
-gcloud config set project grantcm
-gcloud beta run deploy $SERVICE \
---region us-central1 \
---source . \
---allow-unauthenticated;
+buildStatus=$?
+
+if [[ $buildStatus -eq 0 ]]; then
+  echo "!!! Build successful !!!"
+
+  # Deploys the web app to Cloud Run
+  gcloud beta run deploy $SERVICE \
+  --project grantcm \
+  --region us-central1 \
+  --source . \
+  --allow-unauthenticated;
+else
+  echo "!!! Build failed. Fix build then redeploy !!!"
+fi
