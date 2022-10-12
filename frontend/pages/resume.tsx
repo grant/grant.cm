04f6ebe28e9b9f7cfbee0792ed2cb0be39432661
Grant Timmerman <744973+grant@users.noreply.github.com>
2022-10-11 19:29:35 -0700

    Update resume.tsx

--- frontend/pages/resume.tsx
+++ frontend/pages/resume.tsx
@@ -2,6 +2,7 @@
 // http://localhost:8080/resume
 export default function Resume() {
   return (
+    // https://console.cloud.google.com/storage/browser/granttimmerman-resume;tab=objects?forceOnBucketsSortingFiltering=false&project=grantcm&prefix=&forceOnObjectsSortingFiltering=false
     <iframe src="https://storage.googleapis.com/granttimmerman-resume/Grant_Timmerman_Resume.pdf" />
   );
 }
