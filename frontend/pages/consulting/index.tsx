0d213045f9f15e02b9df27379008d6d0394f1ff7
Grant Timmerman <granttimmerman@gmail.com>
2025-01-23 00:02:28 -0800

    feat: create overview for consulting prices
    Signed-off-by: Grant Timmerman <granttimmerman@gmail.com>

--- frontend/pages/consulting/index.tsx
+++ frontend/pages/consulting/index.tsx
@@ -6,6 +6,28 @@ import SectionFooter from '../home/section/footer';
 import sectionStyles from '../home/section/sections.module.scss';
 
 export default function Consulting() {
+  const consultingOptions = [
+    {
+      duration: 15,
+      price: 125,
+      title: '15 Min - $125',
+      description: 'Brief advice, quick discussions, problem scoping',
+    },
+    {
+      duration: 30,
+      price: 225,
+      title: '30 Min - $225',
+      description: 'Technical analysis, market dynamics, initial strategy',
+    },
+    {
+      duration: 60,
+      price: 400,
+      title: '60 Min - $400',
+      description:
+        'Strategic consulting, market research, complex problem solving',
+    },
+  ];
+
   return (
     <Layout>
       <Head key="head">
@@ -16,15 +38,7 @@ export default function Consulting() {
         <div className={styles.centerBox}>
           <h1 className={styles.name}>Timmerman Consulting, LLC</h1>
 
-          <div
-            style={{
-              textAlign: 'center',
-              maxWidth: '440px',
-              margin: 'auto',
-              padding: '20px',
-              color: '#333',
-            }}
-          >
+          <div className="text-center max-w-lg mx-auto p-5 text-gray-800">
             <p>
               Timmerman Consulting, LLC specializes in designing and developing
               custom software solutions and websites tailored to meet the unique
@@ -32,22 +46,22 @@ export default function Consulting() {
             </p>
           </div>
 
-          <p
-            style={{
-              textAlign: 'center',
-              maxWidth: '440px',
-              margin: 'auto',
-              padding: '20px',
-              color: '#333',
-            }}
-          >
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            {consultingOptions.map(option => (
+              <a
+                key={option.duration}
+                href={`/consulting/${option.duration}`}
+                className="p-4 rounded-lg shadow hover:shadow-md transition bg-white bg-opacity-10 border-6 border-gray-800"
+              >
+                <h3 className="font-bold text-lg">{option.title}</h3>
+                <p className="">{option.description}</p>
+              </a>
+            ))}
+          </div>
+
+          <p className="text-center max-w-lg mx-auto p-5 text-gray-800">
             If you are interested in consulting services, please contact{' '}
-            <span
-              style={{
-                fontFamily: 'monospace',
-                fontWeight: 'bold',
-              }}
-            >
+            <span style={{fontFamily: 'monospace', fontWeight: 'bold'}}>
               granttimmerman at gmail
             </span>
             .
